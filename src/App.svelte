c3c50ab04d6ea80e5ff221ed3c0f79a459842950
ritasidorska <m.sidorskaya@innopolis.university>
2023-03-13 11:29:08 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+</script>
+
+<main>
+    <header>
+        <div>
+            <h1>Margarita Sidorskaya</h1>
+        </div>
+    </header>
+
+</main>
+
+<style>
+    header {
+        text-align: center;
+        margin: auto;
+        width: 50%;
+    }
+
+    header div {
+        margin: 150px 150px 50px;
+    }
+</style>
